b95263b09bf4fdd824506e4b2aec2d98e302bdc3
redto0 <aboccacc@umich.edu>
2025-01-14 21:55:04 -0500

    "Fixed" file handling

--- main.cpp
+++ main.cpp
@@ -43,16 +43,24 @@ int main() {
     Polynomial poly;
 
     std::string line;
+    int frame_num = 0;
 
     // reading the file
     //std::ifstream MyReadFile("/home/alex/IdeaProjects/Derek Cant Clean Data/src/dereksDataCleaned.txt");
-    std::ifstream MyReadFile("/home/alex/CLionProjects/untitled/Data/DataDecent.txt");
+    std::ifstream MyReadFile("/home/alex/CLionProjects/untitled/Data/Datav1.txt");
     // std::ifstream MyReadFile("C:/Users/Alex Boccaccio/CLionProjects/untitled/Data/Datav1.txt");
 
     if (!MyReadFile.is_open()) {
         std::cerr << "Error: Could not open MyReadFile: " << strerror(errno);
         return 1;
     }
+
+    std::ofstream MyWriteFile("/home/alex/CLionProjects/untitled/Data/outPut.txt");
+
+    if (!MyWriteFile.is_open()) {
+        std::cout << "Error: Could not open MyWriteFile: " << strerror(errno);
+    }
+
     std::vector<float> vect;
     std::vector<std::string> stringer;
     while (!MyReadFile.eof()) {
@@ -82,20 +90,22 @@ int main() {
         poly = Polynomial(vect);
 
         // interval for polynomial
-        float max = 10;
-        float interval = 0.25;
+        float max = 540;
+        float numberOfIntervals = 38;
         float start = 0;
+        float interval = (max - start) / numberOfIntervals;
         bool outputPrinted = false;
+        std::string stringout;
 
         for (float i = start; i <= max; i += interval) {
             // if (poly != null){
             if (1) { // Always true condition can be removed
                 // do left poly math;
                 // <y, -x>
-                float dx = poly.polyDerivative(i);
+                float dy = poly.polyDerivative(i);
                 // calculate dx where dy is 1
                 // because I don't want to use a wrapper class
-                float dy = 1;
+                float dx = 1;
                 // set dy to 1
                 float l = std::sqrt(dx * dx + dy * dy); // find the magnitude
                 dx /= l;  // normalize
@@ -105,22 +115,32 @@ int main() {
                 float y = poly.poly(x);     // define y
                 x = (x + 7 * dy);           // project x
                 y = (y - 7 * dx);           // project y
-                std::cout << x << ", " << y << std::endl;
-                stringer.push_back(std::to_string(x) + ", " + std::to_string(y) +"\n");
-                outputPrinted = true;
+                //std::cout << x << ", " << y << std::endl;
+                float lengthCheck = sqrt(pow(x - i, 2)  + pow(y - poly.poly(i), 2));
+                if (lengthCheck < 7.5 && lengthCheck > 1 ) {
+                    std::cout   << " valid " << x << ", " << y << "   length: "<< lengthCheck << std::endl; ;
+                    MyWriteFile << " valid " << x << ", " << y << "   length: "<< lengthCheck<< std::endl;
+                } else {
+                    // std::cout   << "invalid " << x << ", " << y << std::endl; ;
+                    // MyWriteFile << "invalid " << x << ", " << y << std::endl;
+                }
+                //std::cout << stringout << std::endl;
+                //MyWriteFile << stringout << std::endl;
+                // stringer.push_back(std::to_string(x) + ", " + std::to_string(y) +"\n");
+                // outputPrinted = true;
             }
         }
-        std::cout << "new polynomial: " << "\n";
+        std::cout << "new polynomial: "  + std::to_string(frame_num)<< "\n";
+        MyWriteFile << "new polynomial: " + std::to_string(frame_num) << "\n";
+        frame_num++;
         if (!outputPrinted) {
             std::cerr << "No output was printed, possibly due to invalid polynomial data." << std::endl;
+            MyWriteFile << "No output was printed, possibly due to invalid polynomial data." << std::endl;
+
         }
         //std::stringstream ss(line);
     }
-    std::ofstream MyWriteFile("/home/alex/CLionProjects/untitled/Data/outPut.txt");
 
-    if (!MyWriteFile.is_open()) {
-        std::cout << "Error: Could not open MyWriteFile: " << strerror(errno);
-    }
     int i = 0;
     while ( i < stringer.size()) {
         MyWriteFile << stringer[i] << "";
